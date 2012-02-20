759d84c2023ccc77f0903a382b0383c7444193bb
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-21 00:09:57 +0700

    Lesson 1 - Downloading the Tools
    In this tutorial I cover how to download the tools necessary (xcode) to begin programming on the mac:
    http://www.youtube.com/watch?v=4BVD8JsOiDI
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-1/Lesson-1/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Lesson-1
+//
+//  Created by Nicolas Georget on 2/20/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+  @autoreleasepool {
+    // insert code here...
+    NSLog(@"Hello, World!");
+  }
+  
+  return 0;
+}
+
