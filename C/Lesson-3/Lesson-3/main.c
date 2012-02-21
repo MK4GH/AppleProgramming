d77de94451ba8dc79c0c4e264492fd813ec3703a
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-21 22:30:48 +0700

    Lesson 3 - Basics & printf
    In this tutorial I cover the basic structure of a program as well as the printf function.
    http://www.youtube.com/watch?v=MqU2v2ftqPU
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-3/Lesson-3/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  Lesson-3
+//
+//  Created by Nicolas Georget on 2/21/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+// Preprocessor directives
+#include <stdio.h>
+
+// Main function
+int main(int argc, const char * argv[]) {
+
+  // here's a comment line... Compiler doesn't care
+  
+  /* Here's a comment multi-line
+    You can type what you want */
+  
+  printf("Hello, World!\n");
+  printf("Second Line");
+  return 0;
+  
+}
+
