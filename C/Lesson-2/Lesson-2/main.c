e22fe19cca18f375e61d47cd1b768960ee9a71d6
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-21 22:17:18 +0700

    Lesson 2 - Using Xcode
    In this tutorial we explore how to create a new project and begin to use Apple's Xcode IDE.
    http://www.youtube.com/watch?v=majuG_JNCbk
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-2/Lesson-2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Lesson-2
+//
+//  Created by Nicolas Georget on 2/21/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+
+}
+
