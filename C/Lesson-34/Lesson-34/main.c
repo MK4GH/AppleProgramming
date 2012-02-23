3fb60eee18cf62086ecf78cbfe732e361d03f633
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-23 18:08:15 +0700

    Lesson 34 - Linked Lists (Part 2/3)
    Part 2 of Linked Lists in C. Subscribe to AppleProgramming for frequent video updates.
    http://youtu.be/4gFrfgfzYJg
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-34/Lesson-34/main.c
@@ -0,0 +1,44 @@
+//
+//  main.c
+//  Lesson-34
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedLists.h"
+
+int main(int argc, const char * argv[]) {
+  
+  struct Person *head, *tail, *current;
+  head = NULL;
+  int i;
+  
+  for (i = 1; i <= 4; i++) {
+    // malloc function allocates a specified memory
+    // that get by the function sizeof that returns
+    // how much memory the struct Person needs
+    current = malloc(sizeof(struct Person));
+    
+    // the '->' means that the current pointer has the
+    // address of the struct. And with the -> will point
+    // to the elements inside the struct
+    current->num = i;
+    printf("Enter a name: ");
+    fgets(current->name, kMaxLength, stdin);
+    RemoveReturn(current->name);
+    
+  }
+  
+  printf("Program ends...");
+  return 0;
+}
+
+// RemoveReturn function
+void RemoveReturn (char *name) {
+  int length = strlen(name);
+  name[length - 1] = '\0';
+}
