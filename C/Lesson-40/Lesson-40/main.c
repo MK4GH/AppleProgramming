fd86a0fbb951dc5a0867d04538958e16a4218722
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-23 22:14:39 +0700

    Lesson 40 - Running C Programs in Terminal
    How to compile and run C programs in Terminal on Mac OS. IMPORTANT: Since our file path is set to the user's home folder by default, these programs will be compiled and saved in your home folder. So you can either change your directory or delete/move the programs that are saved in your home folder.
    http://youtu.be/0xn2ZE3Y90A
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-40/Lesson-40/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Lesson-40
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+  printf("Sweet World!\n");
+  
+  return 0;
+}
+
