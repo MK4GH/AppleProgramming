11d771d0d419d774b229fe4353851073459e204a
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-24 00:24:46 +0700

    Lesson 1 - Starting out
    The introduction to the Objective-C series. Tutorial for downloading Xcode: http://www.youtube.com/watch?v=4BVD8JsOiDI
    (This is also the Beginning C series that new programmers should start with).
    http://youtu.be/1jDS9KCYwFI
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ Objective-C/Lesson-1/Lesson-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Lesson-1
+//
+//  Created by Nicolas Georget on 2/24/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+  @autoreleasepool {
+      
+    // insert code here...
+    NSLog(@"Hello, World!");
+      
+  }
+  
+    return 0;
+}
+
