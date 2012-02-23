bbfef81ec17402e281941f2add312126322e7d00
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-23 20:12:50 +0700

    Lesson 35 - Linked Lists (Part 3/3)
    Part 3 of Linked Lists in C. Subscribe to AppleProgramming for frequent video updates.
    http://youtu.be/Bvyjo4E-jKI
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-35/Lesson-35/LinkedLists.h
@@ -0,0 +1,25 @@
+//
+//  LinkedLists.h
+//  Lesson-35
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#ifndef Lesson_35_LinkedLists_h
+#define Lesson_35_LinkedLists_h
+
+// #defines
+#define kMaxLength 200
+
+// Structs
+struct Person {
+  int num;
+  char name[kMaxLength];
+  struct Person *next;
+};
+
+// Prototypes
+void RemoveReturn (char *name);
+
+#endif
