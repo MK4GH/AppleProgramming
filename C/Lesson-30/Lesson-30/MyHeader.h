424ea5c46aefb4e8200ec81d203a2be5a2302ab1
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-23 15:15:57 +0700

    Lesson 30 - Creating header files
    How to create your own header files.
    http://youtu.be/TFtM7D0UoYU
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-30/Lesson-30/MyHeader.h
@@ -0,0 +1,19 @@
+//
+//  MyHeader.h
+//  Lesson-30
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#ifndef Lesson_30_MyHeader_h
+#define Lesson_30_MyHeader_h
+
+// Constant variable
+#define kMaxLength 50
+
+// function prototype
+void MyFunction();
+
+
+#endif
---
+++ C/Lesson-30/Lesson-30/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Lesson-30
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "MyHeader.h"
+
+int main(int argc, const char * argv[]) {
+
+  printf("kMaxLength defined in the header is %i\n", kMaxLength);
+  
+  return 0;
+}
+
