552cc576187c76e9f10703b9280274c9f423e7fe
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-23 14:09:43 +0700

    Lesson 19 - Intro to Input
    This tutorial is an introduction to input in C. More tutorials for input on the way.
    http://youtu.be/zJ9YkjNsD0c
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ C/Lesson-19/Lesson-19/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  Lesson-19
+//
+//  Created by Nicolas Georget on 2/23/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+  int myInt;
+  
+  scanf("%i", &myInt);
+  printf("You typed %i\n", myInt);
+  
+  return 0;
+}
+
