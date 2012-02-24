e96c196445db9c719f93254557c6614a6c1a4da8
Nicolas Georget <nicolas.georget@gmail.com>
2012-02-24 17:54:55 +0700

    Lesson 3 - @interface
    An introduction to classes in Objective-C and using the @interface section.
    http://youtu.be/UT58TsE-x-4
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ Objective-C/Lesson-3/Lesson-3/Rectangle.m
@@ -0,0 +1,13 @@
+//
+//  Rectangle.m
+//  Lesson-3
+//
+//  Created by Nicolas Georget on 2/24/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import "Rectangle.h"
+
+@implementation Rectangle
+
+@end
---
+++ Objective-C/Lesson-3/Lesson-3/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Lesson-3
+//
+//  Created by Nicolas Georget on 2/24/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+
+  @autoreleasepool {
+      
+    // insert code here...
+    NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
