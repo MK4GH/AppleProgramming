3d35d79a0d32148740042c05ede0ccfa2c418a41
Nicolas Georget <nicolas.georget@gmail.com>
2012-03-14 12:46:53 +0700

    Lesson 23 - Auto Layout
    How to use Cocoa's new Auto Layout feature in Lion.
    http://youtu.be/mhvKKBha0fw
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ Cocoa/Lesson-23/Lesson-23/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Lesson-23
+//
+//  Created by Nicolas Georget on 3/14/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
