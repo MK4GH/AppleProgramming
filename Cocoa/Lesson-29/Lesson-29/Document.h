b30849f5ef964375437786d318463b092d8c7c23
Nicolas Georget <nicolas.georget@gmail.com>
2012-03-14 18:24:56 +0700

    Lesson 29 - Core Data (Part 1/2)
    How to model objects using the Core Data model editor.
    http://youtu.be/CcsLsmLNj3c
    
    Signed-off-by: Nicolas Georget <nicolas.georget@gmail.com>

---
+++ Cocoa/Lesson-29/Lesson-29/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Lesson-29
+//
+//  Created by Nicolas Georget on 3/14/12.
+//  Copyright (c) 2012 nic-o.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+
+@end
